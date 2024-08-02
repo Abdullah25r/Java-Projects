61cf18a56a0ca0dffd6504cbaaf0d6dcbf64d3ba
Abdullah Rasheed <bhatti253r@gmail.com>
2024-08-02 21:23:45 +0500

    docs: bubble sort

--- LinearSearch/src/Test.java
+++ LinearSearch/src/Test.java
@@ -1,19 +1,29 @@
-public class Test {
-    {
-        System.out.println("this is instance block");
-    }
-    static {
-        System.out.println("this is static block");
-    }
-    Test(){
-        System.out.println("this is a constructor");
-    }
+import java.io.*;
+import java.util.Arrays;
 
-    void printLoop(){
-        System.out.println("print for loop");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-            
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+        int Id = 0;
+//        File file = new File("test.txt");
+//        FileWriter fileWriter = new FileWriter(file);
+//        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        // same as above
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("text.txt")));
+
+        bufferedWriter.write(Id++ + " Mohsin bhatti\n");
+        bufferedWriter.write(4 + " Umar bhatti\n");
+        bufferedWriter.close();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("text.txt")));
+        String line;
+        int utilId = 0;
+
+        while ((line = bufferedReader.readLine()) != null) {
+         String[] arr = line.split(" ");
+          utilId = Integer.parseInt(arr[0]);
         }
+        System.out.println(utilId);
+        bufferedReader.close();
+    // that is now working. about 70 percent is done;
     }
 }
