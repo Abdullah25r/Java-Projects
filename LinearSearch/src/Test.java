6fc92e4271e9fda475a5f7b64fdd25943c94f37a
Abdullah Rasheed <bhatti253r@gmail.com>
2024-08-08 09:28:41 +0500

    docs: update linear search

--- LinearSearch/src/Test.java
+++ LinearSearch/src/Test.java
@@ -1,29 +1,54 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-
-        int Id = 0;
-//        File file = new File("test.txt");
-//        FileWriter fileWriter = new FileWriter(file);
-//        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        // same as above
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("text.txt")));
-
-        bufferedWriter.write(Id++ + " Mohsin bhatti\n");
-        bufferedWriter.write(4 + " Umar bhatti\n");
-        bufferedWriter.close();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("text.txt")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("text.txt"),true));
+        writeToFile(bufferedWriter);
+        readFromFile(new File("text.txt"));
+
+
+
+    }
+    // write data to the file
+    public static void writeToFile(BufferedWriter bufferedWriter) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter last name: ");
+        String lastName = sc.nextLine();
+        String fullName = ((firstName.trim()).replaceAll("\\s+", "") + " " + (lastName.trim()).replaceAll("\\s+", "")).toLowerCase();
+        int latestId = getIdFromFile(new File("text.txt"));
+        bufferedWriter.write(++latestId + " " + fullName + "\n" );
+        bufferedWriter.close();
+    }
+
+
+    //read all data from file
+    public static void readFromFile(File file) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] arr = line.split(" ");
+            System.out.println(Arrays.toString(arr));
+        }
+        bufferedReader.close();
+    }
+
+    //get latest id from file
+    public static int getIdFromFile(File file) throws IOException {
         int utilId = 0;
+        String line;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 
         while ((line = bufferedReader.readLine()) != null) {
-         String[] arr = line.split(" ");
-          utilId = Integer.parseInt(arr[0]);
+            String[] arr = line.split(" ");
+            utilId = Integer.parseInt(arr[0]);
         }
-        System.out.println(utilId);
         bufferedReader.close();
-    // that is now working. about 70 percent is done;
+        return utilId;
+
     }
 }
